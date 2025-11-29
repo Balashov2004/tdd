5c5c56b37736f3452a405b9bc19848486ef85eea
Balashov2004 <sashabalashov2019@gmail.com>
2025-11-29 16:45:27 +0500

    fix test

--- cs/TagsCloudVisualizationTests/SpiralPointGeneratorTests.cs
+++ cs/TagsCloudVisualizationTests/SpiralPointGeneratorTests.cs
@@ -10,14 +10,17 @@ namespace TagsCloudVisualizationTests;
 public class SpiralPointGeneratorTests
 {
     private SpiralPointGenerator spiral;
+    private AppSettings defaultSettings;
     private readonly Point center = new Point(500, 500);
     
     [SetUp]
     public void SetUp()
     {
-        spiral = new SpiralPointGenerator(center, 0.1);
+        defaultSettings = new AppSettings(spiralDensity: 0.1); 
+        spiral = new SpiralPointGenerator(center, defaultSettings.SpiralDensity);
     }
     
+
     [Test]
     public void FirstPointInCenter_Test()
     {
@@ -25,9 +28,10 @@ public class SpiralPointGeneratorTests
         var distance = Math.Sqrt(
             Math.Pow(firstPoint.X - center.X, 2) + Math.Pow(firstPoint.Y - center.Y, 2)
         );
+        
         Assert.That(distance, Is.LessThan(1.0));
     }
-    
+
     [Test]
     public void AveragePointsAreNearWithCenter_Test()
     {
@@ -38,5 +42,22 @@ public class SpiralPointGeneratorTests
         
         Assert.That(avgX, Is.InRange(center.X - 5, center.X + 5));
         Assert.That(avgY, Is.InRange(center.Y - 5, center.Y + 5));
+    }
+    
+    
+    [Test]
+    public void DistanceIncreasesMonotonically_Test()
+    {
+        var points = spiral.GeneratePoints().Take(50).ToList();
+        var previousDistance = 0.0;
+        
+        foreach (var point in points.Skip(1))
+        {
+            var currentDistance = Math.Sqrt(
+                Math.Pow(point.X - center.X, 2) + Math.Pow(point.Y - center.Y, 2)
+            );
+            Assert.That(currentDistance, Is.GreaterThanOrEqualTo(previousDistance - 0.01));
+            previousDistance = currentDistance;
+        }
     }
 }
